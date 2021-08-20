bab3fd5175d821df3bbf5b9c71867d6689273f06
sgeinitz <69814800+sgeinitz@users.noreply.github.com>
2021-08-20 13:14:26 -0600

    Update activity01_test.py
    Created expected_string var so that GitHub workflow output obscures the solutions

--- activity01_test.py
+++ activity01_test.py
@@ -7,12 +7,15 @@ class TestActivity01Functions(unittest.TestCase):
 
     def testHelloWorldNormal(self):
         """ Confirm that helloWorldNormal() returns the correct string """
-        self.assertEqual('hello world', helloWorldNormal())
+        expected_string = 'hello world'
+        self.assertEqual(expected_string, helloWorldNormal())
 
     def testHelloWorldInReverse(self):
         """ Confirm that helloWorldInReverse() returns the correct string """
-        self.assertEqual('hello world'[::-1], helloWorldInReverse())
+        expected_string = 'hello world'[::-1]
+        self.assertEqual(expected_string, helloWorldInReverse())
 
     def testHelloWorldTimesX(self):
         """ Confirm helloWorldTimesX() returns correct string for given input """
-        self.assertEqual('hello world' * 3, helloWorldTimesX(3))
+        expected_string = 'hello world' * 3
+        self.assertEqual(expected_string, helloWorldTimesX(3))
